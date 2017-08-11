2411c329e6bb7c95a785589da5ae3a310c54ba8b
Omar Moreno <ormoreno@unipanamericana.edu.co>
2017-08-11 05:06:52 -0500

    last update of web page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year = 2017;
   constructor() { }
 
   ngOnInit() {
+    const d = new Date();
+    this.year = d.getFullYear();
   }
 
 }
